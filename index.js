d014e792c4c3f029b214bfe04e203c4a05a69a1e
marksix <82834747+marksix@users.noreply.github.com>
2021-07-04 23:40:18 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,49 @@
+/*
+ express + http-proxy-middleware 代理请求
+ */
+const express = require('express');
+const timeout = require('connect-timeout');
+const proxy = require('http-proxy-middleware');
+const app = express();
+// 本服务端口
+const PORT = 9527
+// cookie写入地址
+const cookieUrl = '127.0.0.1'
+// PORT 服务端口
+const  kandao =  { 
+  target : 'http://125.44.97.88:8900',
+  changeOrigin: true,
+  cookieDomainRewrite: {
+    "*": cookieUrl // 把相应的 cookie 域都设置成 localhost
+  },
+  ws: true,
+  withCredentials: true,
+ }
+ 
+// 超时时间
+const TIME_OUT = 30 * 1e3;
+ 
+// 设置端口
+ 
+// 设置超时 返回超时响应
+app.use(timeout(TIME_OUT));
+//设置跨域访问
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Headers", "DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Access-Control-Allow-Headers,Authorization,Origin,Accept,Power-By,x-token-id");
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("accept",'image/webp,image/apng,image/*,*/*;q=0.8')
+  // res.header("Content-Type", "application/json;charset=utf-8");
+  next();
+});
+app.use((req, res, next) => {
+  if (!req.timedout) next();
+});
+// 看到代理
+app.use(proxy('/cms', { ...kandao }));
+ 
+// 监听端口
+app.listen(PORT, () => {
+  console.log(`启动代理服务器=> http://127.0.0.1:${PORT}`);
+});
